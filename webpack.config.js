38150fc7fc72f18cbf0837f2cd129b2fe334a6c0
Vitalii Khatiushyn <fend0r86@gmail.com>
2020-01-16 04:20:52 -0500

    updated npm packages, added missing files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     //...
     mode: 'development',
     devServer: {
       contentBase: path.join(__dirname, 'dist'),
       port: 9000
-    }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({title:'snake',template: 'index.html'})
+    ]
   };
